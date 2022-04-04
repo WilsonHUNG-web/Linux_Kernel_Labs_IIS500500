9a1ca663cabf56d33b99179c6a81c6478d3592ab
Enderman <83426136+WilsonHUNG-web@users.noreply.github.com>
2022-04-05 02:43:01 +0800

    Update mycall.c

--- mycall/mycall.c
+++ mycall/mycall.c
@@ -1,10 +1,15 @@
 #include <linux/kernel.h>
+#include <asm/uaccess.h>
 
 char text[101];
 
 asmlinkage long sys_mycall(char* str, int count)
 {
   int i = 0;
+
+  //check memory access violaion of READ, memory not in user space -> return non-zero -> return -efault
+  if(!(access_ok(VERIFY_READ, str, count)))
+	return -EFAULT;
 
   if(count<100)
 	{
